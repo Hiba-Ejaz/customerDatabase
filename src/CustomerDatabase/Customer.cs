94a336cb8e6d48073777aba15691c3c2bedf28b6
hiba <hibaejaz06@gmail.com>
2023-06-09 02:31:22 +0300

    customer class added

---
+++ src/CustomerDatabase/Customer.cs
@@ -0,0 +1,13 @@
+namespace CustomerDatabase;
+// 1. Create `Customer` class: This class represents a 
+// customer and should contain properties such as Id, FirstName,
+//  LastName, Email, and Address. 
+// You can decide the suitable data types for each.
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }   
+}
